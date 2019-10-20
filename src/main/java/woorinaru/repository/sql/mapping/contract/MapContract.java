17094236717c37f5b6cbbc5d309a6f856f13ca8b
Changkon Han <chang_kon@hotmail.com>
2019-10-21 01:49:49 +0900

    Delete map contract

--- src/main/java/woorinaru/repository/sql/mapping/contract/MapContract.java
+++
@@ -1,5 +0,0 @@
-package woorinaru.repository.sql.mapping.contract;
-
-public enum MapContract {
-    MAP, NOT_MAP, PARTIAL_MAP;
-}
--- src/main/java/woorinaru/repository/sql/mapping/criteria/ModelCriteria.java
+++
@@ -1,7 +0,0 @@
-package woorinaru.repository.sql.mapping.criteria;
-
-import woorinaru.repository.sql.mapping.contract.ModelContract;
-
-public interface ModelCriteria {
-    boolean isSatisfiedBy(ModelContract contract);
-}

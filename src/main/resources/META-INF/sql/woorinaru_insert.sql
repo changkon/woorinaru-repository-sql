1724a8c4ca07db1b4fafaeeeb8c31feda385a349
Changkon Han <chang_kon@hotmail.com>
2019-12-07 00:54:36 +0900

    Add insert script

---
+++ src/main/resources/META-INF/sql/woorinaru_insert.sql
@@ -0,0 +1,5 @@
+USE 'woorinaru';
+
+INSERT INTO USER VALUES (1, 'Alan Han', 'New Zealand', 'admin@test.com', null, '2019-01-10 10:10:00', null, 'A');
+INSERT INTO USER VALUES (2, 'Henry Walker', 'New Zealand', 'teacher@test.com', 'SUB_LEADER', '2019-01-10 10:10:00', 'DESIGN', 'T');
+INSERT INTO USER VALUES (3, 'Alice Lee', 'New Zealand', 'student@test.com', null, '2019-01-10 10:10:00', null, 'S');
